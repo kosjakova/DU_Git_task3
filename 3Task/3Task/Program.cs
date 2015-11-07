18cff4895a4058b6cadf67408ce91f5bbd025038
Victoria <viktorijabulvane@gmail.com>
2015-11-07 10:08:14 +0200

    Added variables x and y

--- 3Task/3Task/Program.cs
+++ 3Task/3Task/Program.cs
@@ -11,6 +11,33 @@ namespace _3Task
         {
             Console.WriteLine("\n\n Second home work: Git 2015\n Victorija Bulvane IT 3 course.\n\n Third task!");
 
+            int x, y, counter = 0;
+
+            for (int i = 0; i == counter; i++)
+            {
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    Console.Write("\n Enter please first number:  ");
+                    x = Int32.Parse(Console.ReadLine());
+
+                    Console.Write(" Enter please second number: ");
+                    y = Int32.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\n You enter: \n\t    x: {0} \n\t    y: {1}", x, y);
+
+                }
+                catch (Exception error)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\n " + error.Message);
+                    counter++;
+                }
+            }
+
+            Console.ResetColor();
+
             Console.WriteLine("\n\n Press any key to exit!");
 
             Console.ReadKey();
