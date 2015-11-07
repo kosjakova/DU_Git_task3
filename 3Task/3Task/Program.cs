61711c9e75b1121e026eb20416468d9e6d54a5a6
Victoria <viktorijabulvane@gmail.com>
2015-11-07 10:04:28 +0200

    First commit simple console app

---
+++ 3Task/3Task/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n Second home work: Git 2015\n Victorija Bulvane IT 3 course.\n\n Third task!");
+
+            Console.WriteLine("\n\n Press any key to exit!");
+
+            Console.ReadKey();
+        }
+    }
+}
